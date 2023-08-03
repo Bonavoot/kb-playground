f4905bb08e2d333b97d1369aca298c7ce642f16c
Joseph <Jbonventre95@gmail.com>
2023-08-03 18:07:51 -0400

    moving on to phaser, unlucky

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
+import daiba from "./daiba-avatar.png";
 
 function App() {
-  return <>Yo</>;
+  const [player, setPlayer] = useState(null);
+
+  useEffect(() => {
+    kaboom();
+    loadSprite("player", `${daiba}`);
+    const player = add([sprite("player"), pos(250, 0)]);
+    setPlayer(player);
+  }, []);
+
+  return <div className="game-container"></div>;
 }
 
 export default App;
